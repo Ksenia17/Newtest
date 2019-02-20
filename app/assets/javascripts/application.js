65e7507860723ab4776c783ce35e7fb337d9f937
Ksenia17 <mari-mari@ukr.net>
2019-02-21 01:45:34 +0200

    setting devise

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
+
+//= require jquery
+//= require jquery_ujs
+//= require jquery.turbolinks
 //= require turbolinks
+
 //= require_tree .
