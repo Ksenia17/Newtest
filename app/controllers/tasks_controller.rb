dba179c559ea1eb5bf0b62fd389905102577f75a
Ksenia17 <mari-mari@ukr.net>
2019-02-25 00:51:01 +0200

    task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,20 +2,34 @@ class TasksController < ApplicationController
   before_action :authenticate_user! #for devise
 
   def index
-    @tasks = current_user.tasks
+    @projects = Project.find(params[:project_id])
+    @tasks = @projects.tasks
   end
 
   def create
     @project = Project.find(params[:project_id])
 
-    @tasks = current_user.tasks
-
     @task = @project.tasks.build
-    @task .user_id = current_user.user_id
+    @task.user_id = current_user.user_id
 
     if @task.save
-    #  redirect_to 
+      redirect_to project_task_path(project_id: @project.id, id: @task.id), :notice => "New task was successfully created"
     end
 
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @project = Project.find(params[:project_id])
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to project_tasks_path(@project), :notice => "Task was successfully deleted "
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   
-  resources :projects, only:[:new,:create,:index,:show,:edit,:update,:destroy]
-  
   resources :projects, only:[:new,:create,:index,:show,:edit,:update,:destroy] do
     resources :tasks, only:[:new,:create,:index,:show,:edit,:update,:destroy]
   end
