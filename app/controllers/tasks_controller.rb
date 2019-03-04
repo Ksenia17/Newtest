d6f602320857f77af8152162d614a35177da2220
Ksenia17 <mari-mari@ukr.net>
2019-03-04 16:15:37 +0200

    change forms,fixed errors

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,37 +2,31 @@ class TasksController < ApplicationController
   before_action :authenticate_user! #for devise
 
 
-  def index
-    @project = Project.find(params[:project_id])
-    @tasks = @project.tasks
-  end
-
   def edit
-    @project = Project.find(params[:project_id])
+    @project = current_user.projects.find(params[:project_id])
     @task = @project.tasks.find(params[:id])
   end
 
 
   def create
-    
-    @project = current_user.find(params[:project_id])
+    @project = current_user.projects.find(params[:project_id])
     
     @task = @project.tasks.create!(task_params)
     
     respond_to do |f|
-    f.html { redirect_to project_task_url(@project) }
+    f.html { redirect_to project_tasks_url(@project) }
     f.js
     end    
     
   end
 
   def update
-    @project = Project.find(params[:project_id])
+    @project = current_user.projects.find(params[:project_id])
     @task = @project.tasks.find(params[:id])
 
       @task.update_attributes!(task_params)
         respond_to do |f|
-        f.html { redirect_to project_tas_url(@project) }
+        f.html { redirect_to project_tasks_url(@project) }
         f.js
         end
     
@@ -40,14 +34,15 @@ class TasksController < ApplicationController
 
 
   def destroy
-
-    @project = Project.find(params[:project_id])
+   
+    @project =  current_user.projects.find(params[:project_id])
+    
     @task = @project.tasks.find(params[:id])
 
     @task.destroy
 
     respond_to do |f|
-    f.html { redirect_to project_tas_url(@project) }
+    f.html { redirect_to project_tasks_url(@project) }
     f.js
     end
   end
